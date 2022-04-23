abec86997125411a95b0dd9f282960e7c5e5b8dc
Lucas Ginard <contactolucasginard@gmail.com>
2022-04-22 23:34:00 -0400

    implement maps

--- airelibre/modules/home_main/view/HomeViewController.swift
+++ airelibre/modules/home_main/view/HomeViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController,CLLocationManagerDelegate {
 
+    private let manager = CLLocationManager()
     private var homeViewModel:HomeViewModel!
     private var sensorList:[SensorResponse] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+        GMSServices.provideAPIKey("")
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
         callService()
     }
 
@@ -24,6 +41,12 @@ class HomeViewController: UIViewController {
             self.homeViewModel.sensor.forEach{
                 sensor in self.sensorList.append(sensor)
             }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
         }
     }
 }
