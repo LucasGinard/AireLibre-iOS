a36695106b79d002bb4ee9fd80b03cdb7698fc71
Lucas Ginard <contactolucasginard@gmail.com>
2022-05-05 21:08:32 -0400

    implement listeners in labels about , add fonts

--- airelibre/modules/AboutViewController.swift
+++ airelibre/modules/AboutViewController.swift
@@ -9,21 +9,55 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var tvSeeProject: UILabel!
+    @IBOutlet weak var tvLicense: UILabel!
+    @IBOutlet weak var tvCreator: UILabel!
+    @IBOutlet weak var tvIconApp: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setupLabelClicks()
+    }
+        
+    func setupLabelClicks() {
+        let onClickProject = UITapGestureRecognizer(target: self, action: #selector(self.onClickSeeProject(_:)))
+        self.tvSeeProject.isUserInteractionEnabled = true
+        self.tvSeeProject.addGestureRecognizer(onClickProject)
+        
+        let onClickLicense = UITapGestureRecognizer(target: self, action: #selector(self.onClickLicense(_:)))
+        self.tvLicense.isUserInteractionEnabled = true
+        self.tvLicense.addGestureRecognizer(onClickLicense)
+        
+        let onClickCreator = UITapGestureRecognizer(target: self, action: #selector(self.onClickCreator(_:)))
+        self.tvCreator.isUserInteractionEnabled = true
+        self.tvCreator.addGestureRecognizer(onClickCreator)
+        
+        let onClickIconApp = UITapGestureRecognizer(target: self, action: #selector(self.onClickIconApp(_:)))
+        self.tvIconApp.isUserInteractionEnabled = true
+        self.tvIconApp.addGestureRecognizer(onClickIconApp)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func onClickSeeProject(_ sender: UITapGestureRecognizer) {
+        if let url = NSURL(string: "https://github.com/melizeche/AireLibre/#faq"){
+            UIApplication.shared.open(url as URL)
+        }
     }
-    */
-
+    
+    @objc func onClickLicense(_ sender: UITapGestureRecognizer) {
+        if let url = NSURL(string: "https://es.wikipedia.org/wiki/GNU_Affero_General_Public_License"){
+            UIApplication.shared.open(url as URL)
+        }
+    }
+    
+    @objc func onClickCreator(_ sender: UITapGestureRecognizer) {
+        if let url = NSURL(string: "https://www.linkedin.com/in/lucasginard"){
+            UIApplication.shared.open(url as URL)
+        }
+    }
+    
+    @objc func onClickIconApp(_ sender: UITapGestureRecognizer) {
+        if let url = NSURL(string: "https://www.freepik.es/vector-gratis/familia-activa-feliz-caminando-al-aire-libre_7732632.htm"){
+            UIApplication.shared.open(url as URL)
+        }
+    }
 }
