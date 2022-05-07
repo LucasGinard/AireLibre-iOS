1e6905f4b1fedcb1a5b6c76ce952f52997c15fcc
Lucas Ginard <contactolucasginard@gmail.com>
2022-05-06 23:55:57 -0400

    implement buttons twitter and web

--- airelibre/modules/AboutViewController.swift
+++ airelibre/modules/AboutViewController.swift
@@ -13,27 +13,46 @@ class AboutViewController: UIViewController {
     @IBOutlet weak var tvLicense: UILabel!
     @IBOutlet weak var tvCreator: UILabel!
     @IBOutlet weak var tvIconApp: UILabel!
+    @IBOutlet weak var btnTwitter: UIButton!
+    @IBOutlet weak var btnWeb: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupLabelClicks()
     }
         
+    @IBAction func onClickTwitter(_ sender: Any) {
+        let lowerCaseSocialNetworkName = "twitter"
+           let socialNetworkDomain = "com"
+           let userName = "KoaNdeAire"
+           let appURL = URL(string: "\(lowerCaseSocialNetworkName)://user? screen_name=\(userName)")!
+           let application = UIApplication.shared
+         
+           if application.canOpenURL(appURL) {
+              application.open(appURL)
+           } else {
+              let webURL = URL(string:"https://\(lowerCaseSocialNetworkName).\(socialNetworkDomain)/\(userName)")!
+           application.open(webURL)
+           }
+    }
+    
+    @IBAction func onClickWeb(_ sender: Any) {
+        if let url = NSURL(string: "https://airelib.re"){
+            UIApplication.shared.open(url as URL)
+        }
+    }
+    
     func setupLabelClicks() {
         let onClickProject = UITapGestureRecognizer(target: self, action: #selector(self.onClickSeeProject(_:)))
-        self.tvSeeProject.isUserInteractionEnabled = true
         self.tvSeeProject.addGestureRecognizer(onClickProject)
         
         let onClickLicense = UITapGestureRecognizer(target: self, action: #selector(self.onClickLicense(_:)))
-        self.tvLicense.isUserInteractionEnabled = true
         self.tvLicense.addGestureRecognizer(onClickLicense)
         
         let onClickCreator = UITapGestureRecognizer(target: self, action: #selector(self.onClickCreator(_:)))
-        self.tvCreator.isUserInteractionEnabled = true
         self.tvCreator.addGestureRecognizer(onClickCreator)
         
         let onClickIconApp = UITapGestureRecognizer(target: self, action: #selector(self.onClickIconApp(_:)))
-        self.tvIconApp.isUserInteractionEnabled = true
         self.tvIconApp.addGestureRecognizer(onClickIconApp)
     }
     
