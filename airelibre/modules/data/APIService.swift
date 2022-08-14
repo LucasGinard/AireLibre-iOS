9182a1d2f7ff8009e1de56c51ac41ce9d36993b1
Lucas Ginard <contactolucasginard@gmail.com>
2022-08-14 18:36:32 -0400

    safe null pointer service

--- airelibre/modules/data/APIService.swift
+++ airelibre/modules/data/APIService.swift
@@ -20,22 +20,22 @@ class APIService:NSObject {
     
     func apiToGetSensorData(completion : @escaping ([SensorResponse]) -> ()){
         var components = URLComponents(string: "https://rald-dev.greenbeep.com/api/v1/aqi")!
-        do{
-            let parameters = ["start": getDateURL()]
-            components.queryItems = parameters.map { (key, value) in
-                URLQueryItem(name: key, value: value)
-            }
-            components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
-            let request = URLRequest(url: components.url!)
-            URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
-                guard let data = data else { return }
-                let jsonDecoder = JSONDecoder()
-                let sensorpData = try! jsonDecoder.decode([SensorResponse].self, from: data)
-                    completion(sensorpData ?? [])
-            }.resume()
-        }catch{
-            
+        let parameters = ["start": getDateURL()]
+        components.queryItems = parameters.map { (key, value) in
+            URLQueryItem(name: key, value: value)
         }
+        components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+        let request = URLRequest(url: components.url!)
+        URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            do{
+                let sensorpData = try jsonDecoder.decode([SensorResponse].self, from: data)
+                completion(sensorpData)
+            }catch{
+                completion([])
+            }
+        }.resume()
         
     }
 }
